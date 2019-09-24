ffda107c27ff6ef3836b89cdef824ced040ab66f
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-09-24 10:53:49 -0700

    added Request to AA

---
+++ app/admin/requests.rb
@@ -0,0 +1,25 @@
+ActiveAdmin.register Request do
+  menu if: proc{ current_user.permissions == "admin" }
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :user_id, :school_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:user_id, :school_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  index do
+    selectable_column
+    column :school_name
+    column :user_name
+  end
+  
+end
