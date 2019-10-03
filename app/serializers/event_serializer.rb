22d36710f9c4ae191bbe0c407b365cdd18178d52
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-10-02 20:56:55 -0700

    events api is now returning events

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,6 +1,6 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :title, :start, :end, :description
 
-  belongs_to :user, class_name: "User", foreign_key: :organizer_id, optional: true
+  belongs_to :organizer, class_name: "User", foreign_key: :organizer_id, optional: true
   belongs_to :eventable, polymorphic: true
 end
