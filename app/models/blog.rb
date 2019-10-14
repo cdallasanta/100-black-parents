a4aa53a92e26935c90495dfdc283af28ece03b56
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-10-14 13:10:57 -0700

    starting on blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,8 @@
 class Blog < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: :author_id
   belongs_to :blogable, polymorphic: true
+  
+  def to_partial_path
+    'partials/blog'
+  end
 end
