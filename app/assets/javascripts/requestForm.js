1c9c86085d992af925c80e6b89014d8136896875
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-10-14 15:20:28 -0700

    request form appears and hides

---
+++ app/assets/javascripts/requestForm.js
@@ -0,0 +1,38 @@
+closeOnClick = e => {
+  if (e.target.id === "requestGreyBackground") {
+    $('#requestGreyBackground').css('visibility', 'hidden');
+  }
+}
+
+openRequestForm = () => {
+  $('#requestGreyBackground').css('visibility', 'visible');
+}
+
+createRequest = eventData => {
+  // const dist_id = this.props.match.params.dist_id
+  // const school_id = this.props.match.params.school_id
+  // $.ajax({
+  //   type: 'POST',
+  //   url: `/api/districts/${dist_id}/schools/${school_id}/events`,
+  //   data: {
+  //     event: eventData
+  //   },
+  //   context: this,
+  //   success: resp => {
+  //     if (resp.approved) {
+  //       this.setState({
+  //         school_data: {
+  //           ...this.state.school_data,
+  //           events: [...this.state.school_data.events, resp]
+  //         }
+  //       });
+  //     }
+  //     this.setState({showEventForm: false});
+  //   }
+  // })
+}
+
+$(function(){
+  $('.ad').click(openRequestForm);
+  $('#requestGreyBackground').click(closeOnClick);
+});
