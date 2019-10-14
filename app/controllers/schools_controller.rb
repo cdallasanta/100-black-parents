4be4a74f423668f395181780939df1f2718b01ac
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-10-14 13:52:15 -0700

    blogs css now working

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -5,5 +5,11 @@ class SchoolsController < ApplicationController
 
   def show
     @school = School.find(params[:id])
+    @blogs = Blog
+      .where(blogable: @school, blogable_type:"School")
+      .or(Blog.where(blogable: @school.district, blogable_type:"District"))
+    @events = Event
+      .where(eventable: @school, eventable_type:"School", approved:true)
+      .or(Event.where(eventable: @school.district, eventable_type:"District", approved:true))
   end
 end
--- app/javascript/components/BlogCard.js
+++ app/javascript/components/BlogCard.js
@@ -2,14 +2,7 @@ import React from 'react';
 
 const BlogCard = ({blog}) => {
   return (
-    <div className="blog-card">
-      <div className="blog-title">
-        {blog.title}
-      </div>
-      <div className="blog-teaser">
-        {blog.content}
-      </div>
-    </div>
+    
   )
 }
 
