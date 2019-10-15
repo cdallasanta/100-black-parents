2638acedfab16e7cc28ae5aedfac715c00da918c
cdallasanta <chris.dallasanta@gmail.com>
2019-10-15 10:26:27 -0700

    trying to revert back to a working version

--- app/helpers/contact_helper.rb
+++ app/helpers/contact_helper.rb
@@ -2,7 +2,7 @@ module ContactHelper
   def contactOrForm(school)
     if school.site_rep
       return content_tag(:div, "<strong>Rep Name: </strong>#{school.site_rep.name}<br /><strong>Rep Email: </strong>#{school.site_rep.email}".html_safe, id:"contact-info")
-    elsif current_user&.id == school.request&.user_id
+    elsif user_signed_in? && current_user&.id == school.request&.user_id
       return content_tag(:div, content_tag(:div, "Your form has been submitted for approval."), id:"contact-info")
     elsif school.request
       return content_tag(:div, content_tag(:div, "There will be a representative for this location soon!"), id:"contact-info")
@@ -10,7 +10,4 @@ module ContactHelper
       render "partials/request_form"
     end
   end
-
-
-
 end
